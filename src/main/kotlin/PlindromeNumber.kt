6cedafee31f428a6f5f3468f2b33135d84d4adcf
hieudd-teko <hieu.dd@teko.vn>
2021-02-23 08:47:51 +0700

    most water

---
+++ src/main/kotlin/PlindromeNumber.kt
@@ -0,0 +1,19 @@
+fun isPalindrome(x: Int): Boolean {
+    if (x < 0) return false
+    else {
+        var chars = x.toString().toCharArray()
+        var i = 0
+        var j = chars.size - 1
+        var result = true
+        while (i<=j){
+            if(chars[i]==chars[j]){
+                i++
+                j--
+            } else {
+                result = false
+                break
+            }
+        }
+        return result
+    }
+}
---
+++ src/main/kotlin/ZigzagConversion.kt
@@ -0,0 +1,49 @@
+fun convert(s: String, numRows: Int): String {
+    if (numRows == 1) return s
+    var chars = s.toCharArray()
+    val size = chars.size
+    val calCol = (size / (2*numRows - 2)+1) * (numRows - 1)
+    val data = mutableListOf<MutableList<String>>()
+
+    for (j in 0 until numRows) {
+        val rows = mutableListOf<String>()
+        for (i in 0 until calCol) {
+            rows.add("")
+        }
+        data.add(rows)
+    }
+    var i = 0
+    var j = 0
+    var index = 0
+    var positive = true
+    while (index < size) {
+        data[j][i] = chars[index].toString()
+        index++
+        if (i % (numRows - 1) == 0 && j == 0) {
+            positive = true
+            j++
+        } else if (i % (numRows - 1) == 0 && j < numRows-1) {
+            j++
+        } else if (i % (numRows - 1) == 0 && j == numRows-1) {
+            positive = false
+            i++
+            j--
+        } else {
+            if (positive) {
+                i++
+                j++
+            } else {
+                i++
+                j--
+            }
+        }
+    }
+    return data.map { it.filter { it.isNotBlank() }.joinToString("") }.joinToString("")
+}
+
+fun main() {
+    val test = "ABCDE"
+    print(convert(test,4))
+}
+
+//https://leetcode.com/problems/zigzag-conversion/
