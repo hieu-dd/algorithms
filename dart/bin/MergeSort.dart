51a7a0446089ffe7d6a0cfd805bc215f85acac87
14H <hieu.dd@teko.vn>
2021-06-13 16:08:31 -0700

    + update switch case

--- dart/bin/MergeSort.dart
+++ dart/bin/MergeSort.dart
@@ -18,20 +18,18 @@ List<int> mergerSort(List<int> arr) {
 List<int> merge(List<int> left, List<int> right) {
   var result = <int>[];
   while (left.isNotEmpty || right.isNotEmpty) {
-    if (left.isNotEmpty && right.isNotEmpty) {
-      if (left[0] > right[0]) {
-        result.add(right[0]);
-        right.removeAt(0);
-      } else {
-        result.add(left[0]);
-        left.removeAt(0);
-      }
-    } else if (left.isNotEmpty) {
-      result.add(left[0]);
-      left.removeAt(0);
-    } else {
+    if (left.isEmpty) {
       result.add(right[0]);
       right.removeAt(0);
+    } else if (right.isEmpty) {
+      result.add(left[0]);
+      left.removeAt(0);
+    } else if (left[0] > right[0]) {
+      result.add(right[0]);
+      right.removeAt(0);
+    } else {
+      result.add(left[0]);
+      left.removeAt(0);
     }
   }
   return result;
